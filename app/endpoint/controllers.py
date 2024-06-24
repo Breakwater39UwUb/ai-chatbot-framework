872baf5b4c87e1a6288fef374bfbb7694779b3d3
scales890 <132640992+scales890@users.noreply.github.com>
2024-06-24 11:17:37 +0800

    new ch

--- app/endpoint/controllers.py
+++ app/endpoint/controllers.py
@@ -178,11 +178,12 @@ def api():
                 template = Template(intent.speechResponse,
                                     undefined=SilentUndefined)
                 result_json["speechResponse"] = split_sentence(template.render(**context))
-                if result_json["intent"]["name"]:
-                    if result_json["intent"]["name"] == "War Thunder Agent":
-                        if result_json["extractedParameters"]["war_thunder_vehicle"] == "leopard_2a6":
-                            a = War_thunder_vehicles.objects
-                            result_json["speechResponse"].append(a)
+                #if result_json["intent"]["name"] == "War Thunder Agent":
+                if result_json["currentNode"]:
+                    print(result_json["currentNode"])
+                    if result_json["extractedParameters"]["war_thunder_vehicle"] == "leopard_2a6":
+                        a = War_thunder_vehicles.objects
+                        result_json["speechResponse"].append(a)
 
         app.logger.info(request_json.get("input"), extra=result_json)
         return build_response.build_json(result_json)
